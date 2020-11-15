0ca7faf3031d8d36587abefc04f31af503fe1b22
DianArpian <>
2020-11-15 07:13:52 +0700

    Gaji Pegawai

---
+++ src/pbo10/pkg10119916/latihan43/gajipegawai/PBO1010119916Latihan43GajiPegawai.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo10.pkg10119916.latihan43.gajipegawai;
+
+import java.util.Scanner;
+/**
+ *
+ * @author korik
+ */
+public class PBO1010119916Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GajiPegawai gp = new GajiPegawai();
+        System.out.println("### Data Gaji Karyawan PT. KAKATU ###");
+        System.out.println("-----------------------------------");
+        Scanner scn1 = new Scanner(System.in);
+        Scanner scn2 = new Scanner(System.in);
+        Scanner scn3 = new Scanner(System.in);
+        Scanner scn4 = new Scanner(System.in);
+        Scanner scn5 = new Scanner(System.in);
+        
+        System.out.print("Nama Karyawan : ");
+        gp.setNama(scn1.next());
+        System.out.print("Alamat : ");
+        gp.setAlamat(scn2.next());
+        System.out.println("Uang Transport : Rp ");
+        gp.setUangTransport(scn3.nextInt());
+        System.out.print("Uang Tunjangan : Rp ");
+        gp.setUangTunjangan(scn4.nextInt());
+        System.out.print("Gaji Pokok : Rp ");
+        gp.setGajiPokok(scn5.nextInt());
+        
+        System.out.println("TOTAL GAJI : Rp " + gp.totalGaji(250000, 300000, 4500000));
+    }
+    
+}
+       
